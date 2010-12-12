9e5d0c316d22fd97c1f0bec8dcadaf34f2d1988d
Gerard van Helden <drm@melp.nl>
2010-12-12 07:09:40 -0800

    Updated support for Markdown_Extra's 'fenced code blocks'

--- markdown-geshi.php
+++ markdown-geshi.php
@@ -3,7 +3,7 @@
 Plugin Name: Markdown Geshi
 Plugin URI: http://github.com/drm/Markdown_Geshi
 Description: <a href="http://daringfireball.net/projects/markdown/syntax">Markdown syntax</a> with GeSHi code highlighting added.
-Version: 0.1
+Version: 0.2
 Author: Gerard van Helden
 Author URI: http://melp.nl
 */
@@ -19,35 +19,57 @@ class MarkdownGeshi_Parser extends MarkdownExtra_Parser {
      * part is optional.
      */
     public $shebang = '/^\s*#!(\w+)(?:@(\d+))?\s*\n(.*)/s';
+    
+    function hasShebang($code) {
+        if(preg_match($this->shebang, $code, $m)) {
+            return $m;
+        }
+        return false;
+    }
 
     function _doCodeBlocks_callback($matches) {
-        if(preg_match($this->shebang, $matches[1], $m)) {
-            $language = $m[1];
-            $line = (int) (($m[2] > 1) ? $m[2] : 0);
-            $codeblock = $m[3];
-            $highlighter = new GeSHi($this->outdent(trim($codeblock)), $language);
-            $codeblock = $highlighter->parse_code();
-            if($line) {
-                $ret = '<ol';
-                if($line) {
-                    $ret .= ' start="' . $line .'"';
-                }
-                $ret .= '>';
-                preg_match('!^(\s*<pre[^>]+>)(.*)(</pre>)!s', $codeblock, $m);
-                $ret .= preg_replace(
-                    '/.+(\n|$)/', 
-                    '<li>$0</li>', 
-                    $m[2]
-                );
-                $ret .= '</ol>';
-                
-                $ret = $m[1] . $ret . $m[3];
-            } else {
-                $ret = $codeblock;
-            }
-            return "\n\n" . $this->hashBlock($codeblock) . "\n\n";
+        if($m = $this->hasShebang($matches[1])) {
+            return $this->_doGeshi($m);
         } else {
             return parent::_doCodeBlocks_callback($matches);
         }
+    }
+    
+    function _doFencedCodeBlocks_callback($matches) {
+        if($m = $this->hasShebang($matches[2])) {
+            return $this->_doGeshi($m);
+        } else {
+            return parent::_doFencedCodeBlocks_callback($matches);
+        }
+    }
+        
+    function _doGeshi($shebangMatch) {
+        $language = $shebangMatch[1];
+        $line = (int) (($shebangMatch[2] > 1) ? $shebangMatch[2] : 0);
+        $codeblock = $shebangMatch[3];
+        
+        $highlighter = new GeSHi($this->outdent(trim($codeblock)), $language);
+        $highlighted = $highlighter->parse_code();
+        if($line) {
+            preg_match('!^(\s*<pre[^>]+>)(.*)(</pre>)!s', $highlighted, $m);
+
+            $ret = '<ol';
+            if($line) {
+                $ret .= ' start="' . $line .'"';
+            }
+            $ret .= '>';
+            $ret .= preg_replace(
+                '/.+(\n|$)/', 
+                '<li>$0</li>', 
+                $m[2]
+            );
+            $ret .= '</ol>';
+            
+            $ret = $m[1] . $ret . $m[3];
+        } else {
+            $ret = $highlighted;
+        }
+        
+        return "\n\n" . $this->hashBlock($ret) . "\n\n";
     }
 }
